7b8abc09afa5e796fd187eeada0109965d8bd21a
Ibraheem AJ <gai10@uni.brighton.ac.uk>
2015-04-27 16:54:40 +0100

    Persisting Data in DB

--- HaskellWebapp/Handler/Home.hs
+++ HaskellWebapp/Handler/Home.hs
@@ -1,8 +1,7 @@
 module Handler.Home where
 
 import Import
-import qualified Network.HTTP as HP 
-import Control.Applicative ()
+import Control.Applicative
 import Control.Monad()
 import qualified Data.ByteString.Lazy.Char8() 
 import qualified Data.ByteString.Lazy.Char8() 
@@ -18,33 +17,31 @@ import Data.Maybe
 import qualified Data.Text as T 
 import Control.Concurrent (forkIO)
 import Data.Time.ISO8601 -- [docs] := https://hackage.haskell.org/package/iso8601-time-0.1.3/docs/Data-Time-ISO8601.html
-
+import Network.Wai ( responseFile )
 -- /
 -- Get the response
 -- Store it in the database( fork a thread to do this )
 -- return the EmberJS webapp
+
 getJSON :: IO BS.ByteString
-getJSON = simpleHttp "http://www.phoric.eu/temperature"
+getJSON = simpleHttp $ "http://www.phoric.eu/temperature" -- unsafeperformio
 
 unwrapTemperatures :: Temperatures -> [Temperature]
 unwrapTemperatures (Temperatures {temperatures = xs}) = xs 
 
-getAndStoreTemperatures :: IO ()
-getAndStoreTemperatures = do 
-        d <- (eitherDecode <$> getJSON) :: IO (Either String Temperatures)
-        case d of 
-            Left error -> return ()
-            Right result -> (storeTemperatures result)
+getAndStoreTemperatures :: IO [Handler (Key Temperature)]   
+getAndStoreTemperatures = do  
+        decoded <- (eitherDecode <$> getJSON) :: IO (Either String Temperatures)
+        case decoded of 
+            Right result -> do -- (storeTemperatures $ result) >>= putStrLn "Done"
+                return $ storeTemperatures result
+            Left error -> return $ [] -- putStrLn (pack error)
 
-storeTemperatures :: Temperatures -> IO ()
-storeTemperatures temprs = do 
-    temps <- unwrapTemperatures temprs 
-    (map storeTemperature temps)
-
-storeTemperature :: Temperature -> Handler ()
-storeTemperature (Temperature d t l) = do
-    tempID <- runDB $ insert $ Temperature d t l
-    return () 
+storeTemperatures :: Temperatures -> [Handler (Key Temperature)]
+storeTemperatures temps = map storeTemperature (unwrapTemperatures temps)
+    
+storeTemperature :: Temperature -> Handler (Key Temperature)
+storeTemperature (Temperature d t l) = runDB $ insert $ Temperature d t l
 
 getDate :: Temperature -> Maybe UTCTime
 getDate (Temperature d _ Nothing) = parseISO8601 d
@@ -54,8 +51,8 @@ myDate [] = [Nothing]
 myDate (x:xs) = getDate x : myDate xs 
 
 -- fetch temperature data and store it, then 
-getHomeR :: Handler Text
+getHomeR :: Handler Html
 getHomeR = do
-    _ <- lift (forkIO getAndStoreTemperatures)
-    return "Hello World"
+    _ <- lift getAndStoreTemperatures
+    defaultLayout $(widgetFile "home")
 
